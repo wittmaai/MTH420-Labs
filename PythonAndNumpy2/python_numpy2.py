2f1b7cac67a33f8228bb77a6e02193c9c22b7419
wittmaai <wittmaai@oregonstate.edu>
2022-04-01 12:02:56 -0700

    initial commit

---
+++ PythonAndNumpy2/python_numpy2.py
@@ -0,0 +1,78 @@
+# python_intro.py
+"""Python Essentials: Introduction to Python.
+<Name>
+<Class>
+<Date>
+"""
+
+#Problem 1
+def isolate(a, b, c, d, e):
+
+    raise NotImplementedError("Problem 1 Incomplete")
+
+#Problem 2
+def first_half(string):
+
+    raise NotImplementedError("Problem 2 Incomplete")
+
+
+def backward(first_string):
+
+    raise NotImplementedError("Problem 2 Incomplete")
+
+#Problem 3
+def list_ops():
+
+    raise NotImplementedError("Problem 3 Incomplete")
+
+#Problem 4
+def alt_harmonic(n):
+    """Return the partial sum of the first n terms of the alternating
+    harmonic series. Use this function to approximate ln(2).
+    """
+    raise NotImplementedError("Problem 4 Incomplete")
+
+
+
+def prob5(A):
+    """Make a copy of 'A' and set all negative entries of the copy to 0.
+    Return the copy.
+
+    Example:
+        >>> A = np.array([-3,-1,3])
+        >>> prob4(A)
+        array([0, 0, 3])
+    """
+    raise NotImplementedError("Problem 5 Incomplete")
+
+def prob6():
+    """Define the matrices A, B, and C as arrays. Return the block matrix
+                                | 0 A^T I |
+                                | A  0  0 |,
+                                | B  0  C |
+    where I is the 3x3 identity matrix and each 0 is a matrix of all zeros
+    of the appropriate size.
+    """
+    raise NotImplementedError("Problem 6 Incomplete")
+
+def prob7(A):
+    """Divide each row of 'A' by the row sum and return the resulting array.
+
+    Example:
+        >>> A = np.array([[1,1,0],[0,1,0],[1,1,1]])
+        >>> prob6(A)
+        array([[ 0.5       ,  0.5       ,  0.        ],
+               [ 0.        ,  1.        ,  0.        ],
+               [ 0.33333333,  0.33333333,  0.33333333]])
+    """
+    raise NotImplementedError("Problem 7 Incomplete")
+
+
+def prob8():
+    """Given the array stored in grid.npy, return the greatest product of four
+    adjacent numbers in the same direction (up, down, left, right, or
+    diagonally) in the grid.
+    """
+    raise NotImplementedError("Problem 8 Incomplete")
+
+
