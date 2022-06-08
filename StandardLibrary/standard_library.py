a91177c122c0c911142bd7053af77c93b999b256
wittmaai <102826283+wittmaai@users.noreply.github.com>
2022-06-08 12:59:03 -0700

    Update standard_library.py

--- StandardLibrary/standard_library.py
+++ StandardLibrary/standard_library.py
@@ -1,16 +1,21 @@
 # standard_library.py
 """Python Essentials: The Standard Library.
-<Name>
-<Class>
-<Date>
+Aidan Wittman
+MTH420 Prof. Gibson Spring 2022
+5/30/2022
 """
 
+import calculator
+
+from itertools import combinations
 
 # Problem 1
 def prob1(L):
     """Return the minimum, maximum, and average of the entries of L
     (in that order).
     """
+    return((min(L),max(L),(sum(L)/len(L))))
+
     raise NotImplementedError("Problem 1 Incomplete")
 
 
@@ -19,6 +24,8 @@ def prob2():
     """Determine which Python objects are mutable and which are immutable.
     Test numbers, strings, lists, tuples, and sets. Print your results.
     """
+    print("The mutable objects types are: list, set. \nThe immutable object types are: int, str, tuple.")
+    return()
     raise NotImplementedError("Problem 2 Incomplete")
 
 
@@ -34,6 +41,11 @@ def hypot(a, b):
     Returns:
         The length of the triangle's hypotenuse.
     """
+    a = calculator.product(a,a)
+    b = calculator.product(b,b)
+    w = calculator.sum(a,b)
+    w = calculator.squarey(w)
+    return(w)
     raise NotImplementedError("Problem 3 Incomplete")
 
 
@@ -47,9 +59,36 @@ def power_set(A):
     Returns:
         (list(sets)): The power set of A as a list of sets.
     """
+    m = len(A)
+    mathylist = []
+    for i in range(m+1):
+        mathylist += list(combinations(A, i))
+        i += 1
+    return(mathylist)
     raise NotImplementedError("Problem 4 Incomplete")
 
 
 # Problem 5: Implement shut the box.
 def shut_the_box(player, timelimit):
     """Play a single game of shut the box."""
+
+def main():
+    elements = []
+    n = int(input("You will enter a list of numbers. Please state how many elements will be in this list." ))
+    for i in range(n):
+        nele = int(input(f"Number {i+1}: "))
+        elements.append(nele)
+        i += 1
+    print(elements)
+    x, y, z = prob1(elements)
+    print("Minimum:", x, "Maximum:", y, "Average", z)
+    prob2()
+    print("You will enter two sides of a triangle to calculate its hypotenuse.")
+    x = int(input("First side: "))
+    y = int(input("Second side: "))
+    z = hypot(x,y)
+    print("The hypotenuse of your triangle is: ", z)
+    mathyset = str(input("Please enter a set. I will return its power set. (For example: abc): "))
+    powerset = power_set(mathyset)
+    print(powerset)
+main()
