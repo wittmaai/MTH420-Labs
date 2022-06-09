f9647c024fcc1212406d47608582e2eedeb34982
wittmaai <102826283+wittmaai@users.noreply.github.com>
2022-06-09 15:31:27 -0700

    Create calculator.py

---
+++ StandardLibrary/calculator.py
@@ -0,0 +1,15 @@
+# calculator.py
+"""This file is used in conjunction with Lab 2. It will be imported and its functions will be used
+in Lab 2 in order to calculate the hypotenuse of a triangle.
+"""
+
+import math
+
+def sum(x,y):
+    return x + y
+
+def product(x,y):
+    return x * y
+
+def squarey(x):
+    return math.sqrt(x)
